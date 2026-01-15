78f85c0e7fa59b95fb596a141757a227a0a4d7f2
U5er2457 <133446453+U5er2457@users.noreply.github.com>
2026-01-15 15:44:05 +0100

    Delete javafx_projekt directory

--- javafx_projekt/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.javafx.project.javafx_projekt;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(StudentApplication.class, args);
-    }
-}
